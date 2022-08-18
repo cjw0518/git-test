d0db25697689b1bdef91c2082d5a0d1d5fc3968d
cjw <1748983242@qq.com>
2022-08-18 18:06:17 +0800

    cjw test git

---
+++ src/main/java/com/study/git/TestGit.java
@@ -0,0 +1,14 @@
+package com.study.git;
+
+/**
+ * @author cjw
+ * @version 1.0
+ * 2022/8/18 18:05
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
