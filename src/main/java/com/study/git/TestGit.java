7de07075f20c227721a96fd189744c4b1179b003
cjw <1748983242@qq.com>
2022-08-18 20:18:59 +0800

    hot-fix 1

--- src/main/java/com/study/git/TestGit.java
+++ src/main/java/com/study/git/TestGit.java
@@ -10,6 +10,9 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 
 }
